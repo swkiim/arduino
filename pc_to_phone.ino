bf53471d96d23e99d83f2a65623bdb201366edab
KimSangWoo <swkiim1229@gmail.com>
2022-04-20 18:11:52 +0900

    pc_to_phone
    pc(아두이노)와 휴대폰의 블루투스 통신 : 문자열을 주고받을 수 있는 코드

---
+++ pc_to_phone.ino
@@ -0,0 +1,25 @@
+#include <SoftwareSerial.h>
+SoftwareSerial mySerial(2, 3);
+
+String s0; // string으로 데이터를 수신
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  mySerial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if(Serial.available())
+  {
+    mySerial.write(Serial.read());
+  }
+  if(mySerial.available())
+  {
+    s0 = mySerial.readString();
+    Serial.print(s0); // 문자열을 출력하는 Serial명령어
+//    Serial.write(mySerial.read());
+    Serial.println();
+  }
+}
