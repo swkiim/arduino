a5c246c8e4cfd964710276f9102ad8624a3978a8
KimSangWoo <swkiim1229@gmail.com>
2022-05-12 17:34:21 +0900

    DC_Motor_Serial_Monitor
    DC모터가 작동되는 방향 및 정지 함수를 생성해 시리얼모니터에 특정 숫자를 입력했을 때의 동작확인

---
+++ DC_Motor_Serial_Monitor.ino
@@ -0,0 +1,77 @@
+int pin_DC_N = 6;
+int pin_DC_P = 9;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(pin_DC_N, OUTPUT);
+  pinMode(pin_DC_P, OUTPUT);
+  digitalWrite(pin_DC_P, LOW);
+  digitalWrite(pin_DC_N, LOW);
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  int Speed;
+  int m_con;
+  if(Serial.available() > 0){
+    m_con = Serial.read();
+    if(m_con == '1'){
+      Serial.println("시계방향");
+      cw();
+    }
+    if(m_con == '2'){
+      Serial.println("반시계방향");
+      ccw();
+    }
+    if(m_con == '3'){
+      Serial.println("모터 멈춤");
+      Stop();
+    }
+  }
+}
+
+void cw(){
+  int m_con;
+  int i;
+  for(i = 0; i < 255; i++){
+    m_con = Serial.read();
+    if(m_con == '3'){
+      Stop();
+      Serial.println("모터 멈춤");
+      break;
+    }
+    Motor_CW(i);
+    delay(20);
+  }
+}
+
+void ccw(){
+  int m_con;
+  int i;
+  for(i = 255; i > 0; i--){
+    m_con = Serial.read();
+    if(m_con == '3'){
+      Stop();
+      Serial.println("모터 멈춤");
+      break;
+    }
+    Motor_CCW(i);
+    delay(20);
+  }
+}
+
+void Motor_CW(unsigned char i){
+  digitalWrite(pin_DC_N, LOW);
+  analogWrite(pin_DC_P, i);
+}
+
+void Motor_CCW(unsigned char i){
+  digitalWrite(pin_DC_P, LOW);
+  analogWrite(pin_DC_N, i);
+}
+
+void Stop(void){
+  digitalWrite(pin_DC_N, LOW);
+  digitalWrite(pin_DC_P, LOW);
+}
