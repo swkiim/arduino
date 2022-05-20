5c45b738dfad78f57020690aef43341a73055dcd
KimSangWoo <swkiim1229@gmail.com>
2022-05-20 10:14:01 +0900

    step motor
    stepper.h 라이브러리 없이 스텝모터 작동

---
+++ step_motor.ino
@@ -0,0 +1,23 @@
+byte mp[4] = {4, 5, 6, 7};
+byte p[4] = {B1000, B0100, B0010, B0001};
+byte n[4] = {B0001, B0010, B0100, B1000};
+
+void setup() {
+  // put your setup code here, to run once:
+  for(int k = 0; k < 4; k++){
+    pinMode(mp[k], OUTPUT);
+  }
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  for(int i = 0; i < 512; i++){
+    for(int j = 0; i < 4; j++){
+      for(int k = 0; k < 4; k++){
+        digitalWrite(mp[k], bitRead(p[j], k));
+        delay(3);
+      }
+    }
+  }
+  delay(3000);
+}
