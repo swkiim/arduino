ba6445be68ce930237055f4a9657036db233df32
KimSangWoo <swkiim1229@gmail.com>
2022-05-26 22:22:36 +0900

    dht11
    라이브러리 없이 dht11 온습도 센서를 작동하는 코드

---
+++ dht11.ino
@@ -0,0 +1,51 @@
+#include <Wire.h>
+
+char humi = A0;
+int sensor_val;
+byte dht11_dat[5];
+byte dht11_in;
+byte i;
+
+byte read_dht11_dat(){
+  byte i = 0;
+  byte result = 0;
+
+  for(i = 0; i < 8; i++){
+    while(!digitalRead(humi));
+    delayMicroseconds(30);
+    if(digitalRead(humi) != 0)
+      bitSet(result, 7 - i);
+    while(digitalRead(humi));
+  }
+  return result;
+}
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(humi, OUTPUT);
+  digitalWrite(humi, HIGH);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(humi, LOW);
+  delay(18);
+  digitalWrite(humi, HIGH);
+  delayMicroseconds(5);
+  pinMode(humi, INPUT);
+  delayMicroseconds(200);
+  for(i = 0; i < 5; i++){
+    dht11_dat[i] = read_dht11_dat();
+  }
+  pinMode(humi, OUTPUT);
+  digitalWrite(humi, HIGH);
+  byte dht_check_sum = dht11_dat[0] + dht11_dat[2];
+
+  Serial.print(dht11_dat[0], DEC);
+  Serial.print('%');
+  Serial.print(',');
+  Serial.print(dht11_dat[2], DEC);
+  Serial.println('C');
+  delay(2000);
+}
