d3d4d97f4f5b52010f2ccfa98f5aa878c7026ca0
KimSangWoo <swkiim1229@gmail.com>
2022-05-26 22:28:37 +0900

    dht11_lcd_lib
    라이브러리를 사용해 dht11과 lcd를 활용한 코드

---
+++ dht11_lcd_lib.ino
@@ -0,0 +1,42 @@
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+#include <DHT11.h>
+
+int ths = A0;
+
+DHT11 dht11(ths);
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  lcd.init();
+  lcd.home();
+  lcd.backlight();  
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  int i;
+  float humi, temp;
+  if((i = dht11.read(humi, temp)) == 0){
+    Serial.print("Humidity : ");
+    Serial.println(humi);
+    Serial.print("Temperature : ");
+    Serial.println(temp);
+
+    lcd.setCursor(0, 0);
+    lcd.print("HUMI : ");
+    lcd.print(humi);
+    lcd.print("%");
+    lcd.setCursor(0, 1);
+    lcd.print("TEMP : ");
+    lcd.print(temp);
+    lcd.print("C");
+  }
+  else{
+    Serial.print("Error : ");
+    Serial.print(i);
+  }
+  delay(2000);
+}
