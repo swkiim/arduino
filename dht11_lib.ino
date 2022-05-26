83a7b631c2c021d392d0e768eacfacd4598845e0
KimSangWoo <swkiim1229@gmail.com>
2022-05-26 22:31:15 +0900

    dht11_lib
    dht11 온습도 센서를 라이브러리를 활용하여 작동시키는 코드

---
+++ dht11_lib.ino
@@ -0,0 +1,25 @@
+#include <DHT11.h>
+int ths = A0;
+DHT11 dht11(ths);
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  int i;
+  float humi, temp;
+  if((i = dht11.read(humi, temp)) == 0){
+    Serial.print("Humidity : ");
+    Serial.println(humi);
+    Serial.print("Temperature : ");
+    Serial.println(temp);
+  }
+  else{
+    Serial.print("Error : ");
+    Serial.print(i);
+  }
+  delay(2000);
+}
